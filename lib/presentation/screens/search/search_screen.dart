2d220b050e23e6dbeec596c302af35fdb8abb8a9
Felipe Pérez Leal <seperezl@unal.edu.co>
2023-10-11 00:07:33 -0500

    Search screen updated

--- lib/presentation/screens/search/search_screen.dart
+++ lib/presentation/screens/search/search_screen.dart
@@ -8,11 +8,64 @@ class SearchScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Column(
-          children: [
-            Text('Tripster search page'),
-          ],
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: SearchForm(),
         ),
+      ),
+    );
+  }
+}
+
+class SearchForm extends StatelessWidget {
+  const SearchForm({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Origin',
+            ),
+          ),
+          const SizedBox(height: 20),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Destination',
+            ),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Departure Date',
+                  ),
+                ),
+              ),
+              const SizedBox(width: 20),
+              Expanded(
+                child: TextFormField(
+                  decoration: const InputDecoration(labelText: 'Return Date'),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size(200, 50),
+            ),
+            child: const Text('Search'),
+          ),
+        ],
       ),
     );
   }
