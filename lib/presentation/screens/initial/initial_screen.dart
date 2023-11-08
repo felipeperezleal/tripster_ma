681105ea55bbb9d0150724d566d7931a70f5b28e
Felipe Pérez Leal <seperezl@unal.edu.co>
2023-11-07 20:05:21 -0500

    Initial screen | Text added

--- lib/presentation/screens/initial/initial_screen.dart
+++ lib/presentation/screens/initial/initial_screen.dart
@@ -18,24 +18,51 @@ class _InitialScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final imageHeight = MediaQuery.of(context).size.height * 0.6;
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            'assets/images/icon/tripster.png',
-            fit: BoxFit.contain,
-            height: imageHeight,
+    final theme = Theme.of(context);
+    final imageHeight = MediaQuery.of(context).size.height * 0.5;
+    final padding = MediaQuery.of(context).size.width * 0.1;
+
+    return SafeArea(
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: padding),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Spacer(),
+              Image.asset(
+                'assets/images/icon/tripster.png',
+                height: imageHeight,
+              ),
+              Text(
+                'Hey! Welcome to Tripster',
+                textAlign: TextAlign.center,
+                style: theme.textTheme.headlineMedium?.copyWith(
+                  color: theme.colorScheme.primary,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Where would you like to go?',
+                textAlign: TextAlign.center,
+                style: theme.textTheme.titleMedium?.copyWith(
+                  color: theme.colorScheme.secondary,
+                ),
+              ),
+              const Spacer(),
+              _MainButton(
+                text: 'Log In',
+                color: theme.colorScheme.primary,
+              ),
+              const SizedBox(height: 10),
+              _MainButton(
+                text: 'Sign Up',
+                color: theme.colorScheme.secondary,
+              ),
+              const Spacer(),
+            ],
           ),
-          const _MainButton(
-            text: 'Log In',
-          ),
-          const SizedBox(height: 10),
-          const _MainButton(
-            text: 'Sign Up',
-          ),
-        ],
+        ),
       ),
     );
   }
@@ -43,13 +70,16 @@ class _InitialScreen extends StatelessWidget {
 
 class _MainButton extends StatelessWidget {
   final String text;
+  final Color color;
 
   const _MainButton({
     required this.text,
+    required this.color,
   });
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
     return ElevatedButton(
       onPressed: () {
         if (text == 'Log In') {
@@ -60,6 +90,11 @@ class _MainButton extends StatelessWidget {
       },
       style: ElevatedButton.styleFrom(
         minimumSize: const Size(200, 50),
+        backgroundColor: colors.primary,
+        foregroundColor: colors.onPrimary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
       ),
       child: Text(text),
     );
